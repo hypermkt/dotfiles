741d6251ae8af86559111c303961591094f7d3f4
Makoto Chiba <hypermkt@gmail.com>
2017-11-01 23:34:18 +0900

    Need tmux

--- setup.sh
+++ setup.sh
@@ -44,6 +44,7 @@ brew install peco
 brew install bash-completion
 brew install reattach-to-user-namespace
 brew install gibo
+brew install tmux
 
 # For PHP
 brew install automake autoconf curl pcre bison re2c mhash libtool icu4c gettext jpeg openssl libxml2 mcrypt gmp libevent
