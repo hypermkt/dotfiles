6f93ed60222842b4e6a9ec854ce69c38cbf96c37
Makoto Chiba <hypermkt@gmail.com>
2017-12-09 16:00:07 +0900

    Add terraform

--- setup.sh
+++ setup.sh
@@ -50,6 +50,7 @@ brew install phpbrew
 brew install awscli
 brew install postgresql
 brew install packer
+brew install terraform
 
 # For PHP
 brew install automake autoconf curl pcre bison re2c mhash libtool icu4c gettext jpeg openssl libxml2 mcrypt gmp libevent bzip2
