43ccc5c5ff3827eb435cfd0657e174b5ed080e9b
hypermkt <hypermkt@gmail.com>
2026-02-01 20:33:45 +0900

    feat: add arrow key mappings to nvim-tree for directory navigation

--- nvim/lua/plugin/nvim-tree.lua
+++ nvim/lua/plugin/nvim-tree.lua
@@ -6,7 +6,25 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
+    local function on_attach(bufnr)
+      local api = require('nvim-tree.api')
+
+      local function opts(desc)
+        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      -- デフォルトのマッピングを適用
+      api.config.mappings.default_on_attach(bufnr)
+
+      -- 十字キーでディレクトリを展開・折り畳むための設定
+      -- 右キー: ディレクトリを開く、またはファイルを開く
+      vim.keymap.set('n', '<Right>', api.node.open.edit, opts('Open'))
+      -- 左キー: ディレクトリを閉じる、または親ディレクトリに移動して閉じる
+      vim.keymap.set('n', '<Left>',  api.node.navigate.parent_close, opts('Close Directory'))
+    end
+
     require('nvim-tree').setup({
+      on_attach = on_attach,
       sort_by = 'case_sensitive',
       view = {
         width = 30,
