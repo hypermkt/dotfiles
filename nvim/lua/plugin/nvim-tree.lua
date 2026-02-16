64ba3eac96dd4d8fe16c80ab5adb730333329fa9
hypermkt <hypermkt@gmail.com>
2026-02-17 08:11:06 +0900

    Command + bのキーマップを削除

--- nvim/lua/plugin/nvim-tree.lua
+++ nvim/lua/plugin/nvim-tree.lua
@@ -45,8 +45,5 @@ return {
 
     -- <leader>e でツリーをトグル
     vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { silent = true })
-    
-    -- VS Code like Ctrl+b
-    vim.keymap.set('n', '<C-b>', ':NvimTreeToggle<CR>', { silent = true })
   end
 }
